d91b79ab796bf6586b9bb87ef62ef2809033ef27
aysenuressiz <21060631@stu.omu.edu.tr>
2024-01-20 02:28:41 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:logger/logger.dart';
@@ -217,9 +218,7 @@ class _AndroidSmall2State extends State<AndroidSmall2> {
     // SignUpScreen ekranına geçiş
     Navigator.push(
       context,
-      MaterialPageRoute(
-        builder: (context) => SignUpScreen(),
-      ),
+      MaterialPageRoute(builder: (context) => const RegisterScreen()),
     );
   }
 
@@ -465,90 +464,68 @@ class _AndroidSmall2State extends State<AndroidSmall2> {
   }
 }
 
-class SignUpScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
-  SignUpScreen({super.key});
-
-  void _signUp(BuildContext context) {
-    String email = _emailController.text.trim();
-    String username = _usernameController.text.trim();
-    // ignore: unused_local_variable
-    String password = _passwordController.text;
-
-    // Burada email, username ve password kullanarak yeni bir kullanıcı kaydı yapabilirsiniz.
-    // Kayıt işlemlerini uygulamanıza uygun bir şekilde gerçekleştirmeniz gerekecektir.
-
-    // Örnek olarak, kayıt başarılı olduğunda Ana Akış ekranına yönlendirilebilir.
-    User newUser = User(username: username, email: email, imageUrl: '');
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => HomeScreen(user: newUser),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Sign Up'),
+        title: const Text('Kayıt Ekranı'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextField(
               controller: _emailController,
-              decoration: const InputDecoration(
-                labelText: 'Email',
-                labelStyle: TextStyle(color: Colors.black), // Email metin rengi
-                border: UnderlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.black), // Alt çizgi rengi
-                ),
-              ),
+              decoration: const InputDecoration(labelText: 'E-posta'),
             ),
-            const SizedBox(height: 10),
             TextField(
               controller: _usernameController,
-              decoration: const InputDecoration(
-                labelText: 'Username',
-                labelStyle: TextStyle(color: Colors.black), //  metin rengi
-                border: UnderlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.black), // Alt çizgi rengi
-                ),
-              ),
+              decoration: const InputDecoration(labelText: 'Kullanıcı Adı'),
             ),
-            const SizedBox(height: 10),
             TextField(
               controller: _passwordController,
               obscureText: true,
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                labelStyle: TextStyle(color: Colors.black), // metin rengi
-                border: UnderlineInputBorder(
-                  borderSide:
-                      BorderSide(color: Colors.black), // Alt çizgi rengi
-                ),
-              ),
+              decoration: const InputDecoration(labelText: 'Şifre'),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 16.0),
             ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color.fromARGB(255, 32, 85, 150),
-              ),
-              onPressed: () => _signUp(context),
-              child: const Text('Sign Up'),
+              onPressed: _register,
+              child: const Text('Kayıt Ol'),
             ),
           ],
         ),
       ),
     );
+  }
+
+  void _register() async {
+    var logger = Logger();
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      // Kullanıcı oluşturuldu, ek işlemleri burada yapabilirsiniz.
+      logger.i('Kullanıcı oluşturuldu: ${userCredential.user!.uid}');
+    } catch (e) {
+      logger.i('Kayıt olma hatası: $e');
+    }
   }
 }
 
