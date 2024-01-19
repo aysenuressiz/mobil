0e8e70a1a6e7c8332c59c12aa254b6f89076d83f
aysenuressiz <21060631@stu.omu.edu.tr>
2024-01-19 23:46:02 +0300

    firebase add

--- lib/main.dart
+++ lib/main.dart
@@ -3,18 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:logger/logger.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-void addDataToFirestore() {
-  FirebaseFirestore.instance.collection('your_collection').add({
-    'field1': 'value1',
-    'field2': 'value2',
-  });
-}
+import 'package:mobil/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FigmaToCodeApp());
 }
 
